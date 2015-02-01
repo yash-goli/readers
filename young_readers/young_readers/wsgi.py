b6415e65f38f49fc01ed3d2afaccb5e57d115c60
partha saradhi <parthasaradhi1992@gmail.com>
2015-02-01 17:08:39 +0530

    third push

--- young_readers/young_readers/wsgi.py
+++ young_readers/young_readers/wsgi.py
@@ -1,5 +1,5 @@
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "young_readers.settings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "young_readers.settings.heroku")
 application = Cling(get_wsgi_application())
