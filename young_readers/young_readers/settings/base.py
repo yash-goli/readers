17d4f6a6eab245d75fa9f457625e839ebe62a854
partha saradhi <parthasaradhi1992@gmail.com>
2015-02-01 16:18:05 +0530

    third push

--- young_readers/young_readers/settings/base.py
+++ young_readers/young_readers/settings/base.py
@@ -108,6 +108,7 @@ AUTH_USER_MODEL = 'authentication.Users'
 
 
 import dj_database_url
+DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
