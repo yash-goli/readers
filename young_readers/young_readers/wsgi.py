7ed4ff98137333a720512f6800aaa031e9230237
partha saradhi <parthasaradhi1992@gmail.com>
2015-02-01 16:15:01 +0530

    third push

--- young_readers/young_readers/wsgi.py
+++ young_readers/young_readers/wsgi.py
@@ -1,4 +1,5 @@
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "young_readers.settings.base")
 application = Cling(get_wsgi_application())
